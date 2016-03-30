7dafb7ade63aa3a13754f3c764ed5e1c9b183781
EC2 Default User <ec2-user@ip-10-0-123-219.us-west-2.compute.internal>
2016-03-30 17:26:54 +0000

    tag and mention URLs and stub views

--- koolack_unscaled/urls.py
+++ koolack_unscaled/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^timeline$', views.timeline, name='timeline'),
     url(r'^user/(?P<username>[a-z]+)/$', views.user, name='user'),
+    url(r'^user/(?P<username>[a-z]+)/mentions/$', views.mentions, name='mentions'),
     url(r'^kool/(?P<kool_id>[0-9]+)$', views.kool, name='kool'),
+    url(r'^tag/(?P<tag>[a-z]+)/$', views.tag, name='tag'),
 ]
--- koolack_unscaled/views.py
+++ koolack_unscaled/views.py
@@ -11,5 +11,11 @@ def timeline(request):
 def user(request, username):
     return HttpResponse("user page for %s" % username)
 
+def mentions(request, username):
+    return HttpResponse("mention page for %s" % username)
+
 def kool(request, kool_id):
     return HttpResponse("kool %s" % kool_id)
+
+def tag(request, tag):
+    return HttpResponse("tag %s" % tag)
