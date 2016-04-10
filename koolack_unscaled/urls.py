5fcf965c5ddebd7719cbe51dd1de9240c0dbd318
Jon Richelsen <jrichels@nd.edu>
2016-04-10 17:58:06 +0000

    document login, small HTML formatting

--- koolack_unscaled/urls.py
+++ koolack_unscaled/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
         # if authenticated, redirect to timeline
         # else, display index template
     url(r'^login$', django.contrib.auth.views.login, name='login'),
+        # Django's builtin login, remains an enigma...
+        # redirect after successful login handled by project settings.py
+        # login page specified in project settings.py
     url(r'^logout$', django.contrib.auth.views.logout, {'next_page': '/'}, name='logout'),
     url(r'^register$', views.register, name='register'),
     url(r'^timeline$', views.timeline, name='timeline'),
