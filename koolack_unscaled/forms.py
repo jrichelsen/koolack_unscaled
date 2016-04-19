f43e9363b07336c37c26af3a10704cd223a171a4
Jon Richelsen <jrichels@nd.edu>
2016-04-19 20:26:42 +0000

    add first and last name to user registration

--- koolack_unscaled/forms.py
+++ koolack_unscaled/forms.py
@@ -1,16 +1,37 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 from .models import Kool
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     username = forms.RegexField(
         label='Username', 
         max_length=30, 
         regex=r'^\w+$',
         help_text='Required. 30 characters or fewer. Letters, digits, and _ only.',
         error_message='This value must contain only letters, numbers, and underscores.')
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'password1',
+            'password2',
+        )
+
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
 
 class KoolForm(ModelForm):
     class Meta:
